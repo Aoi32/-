b5a05d85b328bc65969a28c0428223a84e26b62c
Aoba <54072033+Aoi32@users.noreply.github.com>
2020-11-22 14:53:58 +0900

    Add files via upload

---
+++ ao.jl
@@ -0,0 +1,13 @@
+function aoi(a, b, c)
+       if a < b
+	   end
+       println("快晴");
+       if a > b
+	   end
+       println("開成");
+       if a == b
+       for i in 1:c
+       println("カンツォーネ");
+       end
+       end
+       end
